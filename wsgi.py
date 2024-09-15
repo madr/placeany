b495541f10b88bd7a0ff42dc3e4655d58c2d0115
Anders Englöf Ytterström <yttan@fastmail.se>
2024-09-15 21:29:00 +0200

    Cache generation instead of views
    This way, query params are kept cached.

--- wsgi.py
+++ wsgi.py
@@ -10,6 +10,7 @@ app = Flask(__name__)
 
 GREY = "G"
 COLOR = "RGB"
+IMAGE_DIR = "./images"
 
 cache = Cache(config={"CACHE_TYPE": "SimpleCache"})
 
@@ -17,16 +18,17 @@ app = Flask(__name__)
 cache.init_app(app)
 
 
-def get_cropped_image(x, y, grey=False, retries=0):
+@cache.memoize(1)
+def get_cropped_image(x, y, s, grey=False, retries=0):
     """crops a random image from collection"""
     if retries > 10:
         return None
-    options = os.listdir("./images")
+    options = os.listdir(IMAGE_DIR)
     try:
         selection = list(
             filter(
                 lambda i: i in range(0, len(options)),
-                map(int, request.args.getlist("image")),
+                map(int, s),
             )
         )
     except ValueError:
@@ -38,7 +40,7 @@ def get_cropped_image(x, y, grey=False, retries=0):
             im_src = options[selection[0]]
         case _:
             im_src = options[random.choice(selection)]
-    im = Image.open(f"images/{im_src}")
+    im = Image.open(f"{IMAGE_DIR}/{im_src}")
     out = BytesIO()
     max_x, max_y = im.size
     if x > max_x and y > max_y:
@@ -51,8 +53,8 @@ def get_cropped_image(x, y, grey=False, retries=0):
     return out
 
 
-def make_response(x, y, color_mode=COLOR):
-    im = get_cropped_image(x, y, color_mode == GREY)
+def make_response(x, y, s, color_mode=COLOR):
+    im = get_cropped_image(x, y, s, color_mode == GREY)
     if not im:
         return Response(status=401)
     return send_file(im, mimetype="image/webp")
@@ -71,15 +73,13 @@ def bookmarklet():
 
 
 @app.route("/<int:x>/<int:y>")
-@cache.cached(1)
 def generate(x, y):
-    return make_response(x, y, COLOR)
+    return make_response(x, y, request.args.getlist("image"), COLOR)
 
 
 @app.route("/g/<int:x>/<int:y>")
-@cache.cached(1)
 def generate_grey(x, y):
-    return make_response(x, y, GREY)
+    return make_response(x, y, request.args.getlist("image"), GREY)
 
 
 if __name__ == "__main__":
